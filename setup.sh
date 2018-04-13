f4dfe59bd971fd6d6e5f9e2e456ae4fa6b12880e
Joshua Kleveter <joshua.kleveter@gmail.com>
2018-04-13 18:46:34 -0500

    Moved setup.sh to setup. Added Neovim setup code

---
+++ setup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Set up Neovim config
+NVIM_DIRECTORY=~/.config/nvim
+NVIM_INIT=./init.vim
+
+if [ ! -d "$NVIM_DIRECTORY" ]; then
+  mkdir -p $NVIM_DIRECTORY
+fi
+
+if [ -f "$NVIM_DIRECTORY/init.vim" ]; then
+  rm -f $NVIM_DIRECTORY/init.vim
+fi
+
+ln -s ./init.nvim $NVIM_DIRECTORY/init.vim
