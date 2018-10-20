f999805dff703a3036df704c1a2d32d69a5daeb7
Joshua Kleveter <joshua.kleveter@gmail.com>
2018-10-20 10:38:28 -0500

    Added is_executable function to functions.sh

--- scripts/functions.sh
+++ scripts/functions.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
 
+# This function will check for
+# an executable's existence
+# $1 - The name of the executable to check for
+is_executable () {
+  exit_code=0
+
+  command -v "$1" > /dev/null 2>&1
+
+  if [[ ! "$?" -eq 0 ]]; then
+    exit_code=1
+  fi
+
+  return exit_code
+}
+
 # TODO: read_plugin_file Function to read a .txt plugin file
