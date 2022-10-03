b31b842ce223293eb891b1e96de85c385b0f89bd
Joshua Kleveter <madintist@protonmail.com>
2022-10-03 14:28:32 -0500

    Use space instead of leader

--- nvim/init.lua
+++ nvim/init.lua
@@ -7,13 +7,13 @@ vim.opt.relativenumber = true
 vim.opt.updatetime = 100
 
 -- Keybindings
-vim.keymap.set('n', '<leader><Space>', '<cmd>nohlsearch<CR>', { desc = 'Clear highlighted search results' })
-vim.keymap.set('n', '<leader>ff', '<cmd>Telescope find_files<CR>')
-vim.keymap.set('n', '<leader>fg', '<cmd>Telescope live_grep<CR>')
-vim.keymap.set('n', '<leader>fb', '<cmd>Telescope buffers<CR>')
-vim.keymap.set('n', '<leader>fh', '<cmd>Telescope help_tags<CR>')
-vim.keymap.set('n', '<leader>t', '<cmd>NERDTreeToggle<CR>')
-vim.keymap.set('n', '<leader>f', '<cmd>NERDTreeFind<CR>')
+vim.keymap.set('n', '<space>h', '<cmd>nohlsearch<CR>', { desc = 'Clear highlighted search results' })
+vim.keymap.set('n', '<space>ff', '<cmd>Telescope find_files<CR>')
+vim.keymap.set('n', '<space>fg', '<cmd>Telescope live_grep<CR>')
+vim.keymap.set('n', '<space>fb', '<cmd>Telescope buffers<CR>')
+vim.keymap.set('n', '<space>fh', '<cmd>Telescope help_tags<CR>')
+vim.keymap.set('n', '<space>t', '<cmd>NERDTreeToggle<CR>')
+vim.keymap.set('n', '<space>f', '<cmd>NERDTreeFind<CR>')
 
 -- Plugin Settings
 vim.cmd [[silent! colorscheme gruvbox]]
