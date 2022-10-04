f76526b874974e3e6481f7beb9416b1ca2c49111
Joshua Kleveter <madintist@protonmail.com>
2022-10-04 07:16:27 -0500

    Make keybindings non-recursive

--- nvim/init.lua
+++ nvim/init.lua
@@ -8,13 +8,13 @@ vim.opt.relativenumber = true
 vim.opt.updatetime = 100
 
 -- Keybindings
-vim.keymap.set('n', '<space>h', '<cmd>nohlsearch<CR>', { desc = 'Clear highlighted search results' })
-vim.keymap.set('n', '<space>ff', '<cmd>Telescope find_files<CR>')
-vim.keymap.set('n', '<space>fg', '<cmd>Telescope live_grep<CR>')
-vim.keymap.set('n', '<space>fb', '<cmd>Telescope buffers<CR>')
-vim.keymap.set('n', '<space>fh', '<cmd>Telescope help_tags<CR>')
-vim.keymap.set('n', '<space>t', '<cmd>NERDTreeToggle<CR>')
-vim.keymap.set('n', '<space>f', '<cmd>NERDTreeFind<CR>')
+vim.keymap.set('n', '<space>h', '<cmd>nohlsearch<CR>', { desc = 'Clear highlighted search results', noremap = true })
+vim.keymap.set('n', '<space>ff', '<cmd>Telescope find_files<CR>', { noremap = true })
+vim.keymap.set('n', '<space>fg', '<cmd>Telescope live_grep<CR>', { noremap = true })
+vim.keymap.set('n', '<space>fb', '<cmd>Telescope buffers<CR>', { noremap = true })
+vim.keymap.set('n', '<space>fh', '<cmd>Telescope help_tags<CR>', { noremap = true })
+vim.keymap.set('n', '<space>t', '<cmd>NERDTreeToggle<CR>', { noremap = true })
+vim.keymap.set('n', '<space>f', '<cmd>NERDTreeFind<CR>', { noremap = true })
 
 -- Plugin Settings
 vim.cmd [[silent! colorscheme gruvbox]]
