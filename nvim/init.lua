69437465e4824b6bbecd7ef6db78ae2061d494ee
Joshua Kleveter <madintist@protonmail.com>
2022-10-04 07:30:34 -0500

    Move keybindings to keymap.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,18 +1,10 @@
+require('keymap')
 require('load-plugins')
 require('lsp')
 
 -- Vim Core Settings
 vim.opt.relativenumber = true
 vim.opt.updatetime = 100
-
--- Keybindings
-vim.keymap.set('n', '<space>h', '<cmd>nohlsearch<CR>', { desc = 'Clear highlighted search results', noremap = true })
-vim.keymap.set('n', '<space>ff', '<cmd>Telescope find_files<CR>', { noremap = true })
-vim.keymap.set('n', '<space>fg', '<cmd>Telescope live_grep<CR>', { noremap = true })
-vim.keymap.set('n', '<space>fb', '<cmd>Telescope buffers<CR>', { noremap = true })
-vim.keymap.set('n', '<space>fh', '<cmd>Telescope help_tags<CR>', { noremap = true })
-vim.keymap.set('n', '<space>t', '<cmd>NERDTreeToggle<CR>', { noremap = true })
-vim.keymap.set('n', '<space>f', '<cmd>NERDTreeFind<CR>', { noremap = true })
 
 -- Plugin Settings
 vim.cmd [[silent! colorscheme gruvbox]]
