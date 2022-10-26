c72cdc9679c02e33a0eb0ca42e16d136b08d131a
Joshua Kleveter <madintist@protonmail.com>
2022-10-26 13:58:06 -0500

    Make buffer search better

--- nvim/lua/keymap.lua
+++ nvim/lua/keymap.lua
@@ -1,5 +1,5 @@
 -- Core
-vim.keymap.set('n', '<space>cb', '<cmd>bdelete<CR>', { noremap = true })
+vim.keymap.set('n', '<space>bk', '<cmd>bdelete<CR>', { noremap = true })
 vim.keymap.set('n', '<space>h', '<cmd>nohlsearch<CR>', { desc = 'Clear highlighted search results', noremap = true })
 
 -- Scripts
@@ -12,6 +12,6 @@ vim.keymap.set('n', '<space>tf', '<cmd>NvimTreeFindFile<CR>', { noremap = true }
 -- Telescope
 vim.keymap.set('n', '<space>ff', '<cmd>Telescope find_files<CR>', { noremap = true })
 vim.keymap.set('n', '<space>fg', '<cmd>Telescope live_grep<CR>', { noremap = true })
-vim.keymap.set('n', '<space>fb', '<cmd>Telescope buffers<CR>', { noremap = true })
+vim.keymap.set('n', '<space>bf', '<cmd>Telescope buffers<CR>', { noremap = true })
 vim.keymap.set('n', '<space>fh', '<cmd>Telescope help_tags<CR>', { noremap = true })
 vim.keymap.set('n', '<space>ls', '<cmd>Telescope lsp_document_symbols<CR>', { noremap = true })
